7f4f1319f00caf23f90dc3c060374d4691d0af71
AllanCordova <allan.2024@alunos.utfpr.edu.br>
2025-10-03 14:50:14 -0300

    fixed testes finally

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -10,12 +10,12 @@ use Core\Http\Request;
 
 class AuthController extends Controller
 {
-    public function new()
+    public function new(): void
     {
         $this->render('form/login');
     }
 
-    public function create(Request $request)
+    public function create(Request $request): void
     {
         $params = $request->getParam('user');
         $user = User::findByEmail($params['email']);
@@ -24,19 +24,19 @@ class AuthController extends Controller
             Auth::login($user);
             FlashMessage::success('Login realizado com sucesso! Bem-vindo(a), ' . htmlspecialchars($user->name) . '!');
 
-            return $this->redirectTo('/');
+            $this->redirectTo('/');
         } else {
             FlashMessage::danger('E-mail ou senha inválidos. Por favor, tente novamente.');
 
-            return $this->redirectTo('/login');
+            $this->redirectTo('/login');
         }
     }
 
-    public function destroy()
+    public function destroy(): void
     {
         Auth::logout();
 
         FlashMessage::success('Você foi desconectado com segurança.');
-        return $this->redirectTo('/login');
+        $this->redirectTo('/login');
     }
 }
