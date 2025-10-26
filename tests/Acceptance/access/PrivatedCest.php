8e932a7831a0326661d4c48ea7979dc5f23acd71
AllanCordova <allan.2024@alunos.utfpr.edu.br>
2025-10-26 13:12:03 -0300

    Fix validation error display and PrivatedCest tests

--- tests/Acceptance/access/PrivatedCest.php
+++ tests/Acceptance/access/PrivatedCest.php
@@ -30,14 +30,14 @@ class PrivatedCest extends BaseAcceptanceCest
     {
         $I->amOnPage('/decks');
         $I->seeCurrentUrlEquals('/login');
-        $I->see('Login');
+        $I->see('Faça seu Login');
     }
 
     public function unauthenticatedUserCannotAccessProtectedRouteDecksNew(AcceptanceTester $I): void
     {
         $I->amOnPage('/decks/new');
         $I->seeCurrentUrlEquals('/login');
-        $I->see('Login');
+        $I->see('Faça seu Login');
     }
 
     public function unauthenticatedUserCannotAccessProtectedRouteDecksEdit(AcceptanceTester $I): void
@@ -53,7 +53,7 @@ class PrivatedCest extends BaseAcceptanceCest
 
         $I->amOnPage('/decks/' . $deck->id . '/edit');
         $I->seeCurrentUrlEquals('/login');
-        $I->see('Login');
+        $I->see('Faça seu Login');
     }
 
     public function unauthenticatedUserCannotAccessProtectedRouteDecksShow(AcceptanceTester $I): void
@@ -69,14 +69,14 @@ class PrivatedCest extends BaseAcceptanceCest
 
         $I->amOnPage('/decks/' . $deck->id);
         $I->seeCurrentUrlEquals('/login');
-        $I->see('Login');
+        $I->see('Faça seu Login');
     }
 
     public function unauthenticatedUserCannotAccessProtectedRouteCardsNew(AcceptanceTester $I): void
     {
         $I->amOnPage('/cards/new');
         $I->seeCurrentUrlEquals('/login');
-        $I->see('Login');
+        $I->see('Faça seu Login');
     }
 
     public function unauthenticatedUserCannotAccessProtectedRouteCardsEdit(AcceptanceTester $I): void
@@ -105,20 +105,20 @@ class PrivatedCest extends BaseAcceptanceCest
 
         $I->amOnPage('/cards/' . $card->id . '/edit');
         $I->seeCurrentUrlEquals('/login');
-        $I->see('Login');
+        $I->see('Faça seu Login');
     }
 
     public function unauthenticatedUserCannotAccessProtectedRouteAdmin(AcceptanceTester $I): void
     {
         $I->amOnPage('/admin');
         $I->seeCurrentUrlEquals('/login');
-        $I->see('Login');
+        $I->see('Faça seu Login');
     }
 
     public function unauthenticatedUserCannotAccessProtectedRouteLogout(AcceptanceTester $I): void
     {
         $I->amOnPage('/logout');
         $I->seeCurrentUrlEquals('/login');
-        $I->see('Login');
+        $I->see('Faça seu Login');
     }
 }
